ff18a8b5221082e9fe5f3943910a20f2202216a3
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-29 22:14:25 +0800

    Implement Router & Todohead

--- src/components/Todohead.js
+++ src/components/Todohead.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom';
 
 function Todohead() {  
     return (
+        // Header todoList
         <div>
-            <Link to="/" className="todo-head">
+             <Link to="/" className="todo-head">       {/*Implement router that links the home page when clicked */}
                 TO-DO LIST  
             </Link>
         </div>
